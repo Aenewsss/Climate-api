13fcbdaafa6126f4ff088115a3a05a11d2d58f87
Aenã Martinelli <aenamartinelli@gmail.com>
2022-04-14 12:13:00 -0300

    'install_nodemon'

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const app = express()
 const path = require('path')
 const PORT = 3000
 
+const cache = require('./src/redis/cache')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.set('view engine', 'ejs')
-app.set('views', 'src')
+app.set('views', 'src/view')
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/', (req, res) => {
+    res.send(req.body.cityName)
 })
 
 app.listen(PORT, () => console.log(`Server is at http://localhost:${PORT}`))
